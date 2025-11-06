1d56109b74d60c73848f4afd824dc34f4233146c
cl <cl@cl>
2025-11-06 22:53:13 +0100

    added code to write checkpoints

--- testit2.py
+++ testit2.py
@@ -13,6 +13,96 @@ from requests_sse.client import InvalidStatusCodeError
 from my_util import FilenameGen
 import os
 import time
+import datetime
+
+dir_checkpoints = 'streamdata/'
+
+
+def store_checkpoint(*, status=None, data):
+    """
+    No need to provide 'status' when it is the first checkpoint.
+    For following calls, provide data returned from previous call.
+    """
+    outstatus = {}
+    tnow = datetime.datetime.now()
+    fnnew = dir_checkpoints+'/checkpoint_' + tnow.strftime('%Y%m%dT%H%M%S.%f')
+    with open(fnnew,'w') as fout:
+        fout.write(data)
+
+    if status:
+        if 'checkpoint_file' in status:
+            fnold = status['checkpoint_file']
+            # in very unlikely scenario (calls at very frequency), the filenames can be identical
+            if fnnew==fnold:
+                print('Warning: checkpoint filenames identical (old checkpoint file was overwritten, which is not perfect as it could cause loss of checkpointing info in case of crash) -> not deleting')
+            else:
+                try:
+                    os.unlink(fnold)
+                except FileNotFoundError:
+                    pass # ignore FileNotFoundError: failing to delete old checkout should not crash entire program. When resuming from checkpoint, the freshest file is used (based on date/time in filename).
+
+    outstatus['checkpoint_dt']   = tnow
+    outstatus['checkpoint_file'] = fnnew
+    return(outstatus)
+
+def get_stream_data(url = 'https://stream.wikimedia.org/v2/stream/recentchange', cb=None, cb_raw=None):
+    # Info: Wikipedia blocks Python scripts -> gives 403
+    # Fake Firefox -> gives 200
+    kwargs = dict()
+    kwargs['headers'] = dict()
+    kwargs['headers']['User-Agent']='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+    #
+    # FIXME: add code to obtain event id info from checkpoint file
+    checkpoint_throttle_cntr=0
+    last_checkpoint=None
+    last_id = None
+    while True:
+        try:
+            with EventSource(url,**kwargs) as stream:
+                for event in stream:
+                    if event.type == 'message':
+                        try:
+                            change = json.loads(event.data)
+                        except ValueError:
+                            continue # next event
+
+                        # write checkpoints
+                        checkpoint_throttle_cntr+=1
+                        if (checkpoint_throttle_cntr>100):
+                            checkpoint_throttle_cntr=0
+                            # FIXME: is 'id' the correct element in the decoded JSON?
+                            if 'id' in change:
+                                checkpoint_data=str(change['id'])
+                                last_checkpoint = store_checkpoint(status=last_checkpoint, data=checkpoint_data)
+                            else:
+                                print('warning: not storing checkpoint because field id is missing')
+
+                        # this prints the *raw* data
+                        if cb_raw is not None:
+                            cb_raw(event)
+                        """
+                        In very rare cases, expected fields may be missing from the parsed JSON data. On 2025-Nov-05, this script was crashing after a few hours and more than 300000 processed messages with the following exception (crash was reproducible):
+                        Traceback (most recent call last):
+                          File "/home/cl/work/wikitest/./testit.py", line 34, in <module>
+                            if change['user'] == 'Yourname':
+                        KeyError: 'user'
+                        """
+                        # discard canary events
+                        if 'meta' in change:
+                            if 'domain' in change['meta']:
+                                if change['meta']['domain'] == 'canary':
+                                    continue
+
+                        # further processing in user-provided callback function
+                        if cb is not None:
+                            cb(change)
+        except InvalidStatusCodeError as e:
+            print(f'EventSource: HTTP status code: {e.status_code}. Retrying...')
+            time.sleep(5)
+
+#################
+### MAIN CODE ###
+#################
 
 def cb_demo_user(change):
     # print(change)
@@ -42,49 +132,6 @@ def cb_process_raw(event, status):
 
     status['fout'].write(event.data)
     status['fout'].write('\n')
-
-def get_stream_data(url = 'https://stream.wikimedia.org/v2/stream/recentchange', cb=None, cb_raw=None):
-    # Info: Wikipedia blocks Python scripts -> gives 403
-    # Fake Firefox -> gives 200
-    kwargs = dict()
-    kwargs['headers'] = dict()
-    kwargs['headers']['User-Agent']='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
-    #
-    last_id = None
-    while True:
-        try:
-            with EventSource(url,**kwargs) as stream:
-                for event in stream:
-                    if event.type == 'message':
-                        try:
-                            change = json.loads(event.data)
-                        except ValueError:
-                            continue # next event
-
-                        # this prints the *raw* data
-                        if cb_raw is not None:
-                            cb_raw(event)
-                        """
-                        In very rare cases, expected fields may be missing from the parsed JSON data. On 2025-Nov-05, this script was crashing after a few hours and more than 300000 processed messages with the following exception (crash was reproducible):
-                        Traceback (most recent call last):
-                          File "/home/cl/work/wikitest/./testit.py", line 34, in <module>
-                            if change['user'] == 'Yourname':
-                        KeyError: 'user'
-                        """
-                        # discard canary events
-                        if 'meta' in change:
-                            if 'domain' in change['meta']:
-                                if change['meta']['domain'] == 'canary':
-                                    continue
-
-                        # further processing in user-provided callback function
-                        if cb is not None:
-                            cb(change)
-        except InvalidStatusCodeError as e:
-            print(f'EventSource: HTTP status code: {e.status_code}. Retrying...')
-            time.sleep(5)
-
-
 
 
 if __name__=="__main__":
