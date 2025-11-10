72af6360a4fdf56c57ad6bfa09950bb06a9c432a
Christoph Lechner <cl@cl>
2025-11-10 10:34:48 +0100

    improvement of SQL query: does not generate zeros before/after actual events in DB

--- 20251110__gaps_timeseries.md
+++ 20251110__gaps_timeseries.md
@@ -65,3 +65,36 @@ FROM
 SELECT gs_date,gs_hour,q.date,q.hour,COALESCE(q.count,-10) FROM times_without_gap
 LEFT JOIN q ON (q.date=gs_date AND q.hour=gs_hour);
 ```
+
+## Step 4
+Further improvements: series used for gap filling is no longer extending beyond event counts obtained from DB:
+```
+WITH q AS (
+    SELECT
+       DATE(ts_event_meta_dt) AS date, EXTRACT(HOUR FROM ts_event_meta_dt) AS hour,
+       COUNT(*),
+		   -- To determine time range for zero-gap filling in result
+		   -- MIN(ts_event_meta_dt) AS col_min, MAX(ts_event_meta_dt) AS col_max,
+       -- need start of hour, see https://www.postgresql.org/docs/current/functions-datetime.html
+		   DATE_TRUNC('HOUR', MIN(ts_event_meta_dt)) AS col_min_startofhour
+    FROM wiki_change_events_test
+    WHERE
+       event_type='edit' AND event_wiki='dewiki' AND event_title='Wolfgang Amadeus Mozart'
+    GROUP BY
+       DATE(ts_event_meta_dt),EXTRACT(HOUR FROM ts_event_meta_dt)
+    ORDER BY
+       DATE(ts_event_meta_dt),EXTRACT(HOUR FROM ts_event_meta_dt)
+),
+times_without_gap AS(
+SELECT
+	DATE(gs) AS gs_date, EXTRACT(HOUR FROM gs) AS gs_hour
+FROM
+	generate_series((SELECT MIN(col_min_startofhour) FROM q), (SELECT MAX(col_min_startofhour) FROM q), interval '1 hour') AS gs
+)
+SELECT
+    -- column for development purposes in pgadmin
+    gs_date,gs_hour,q.date,q.hour,COALESCE(q.count,-10) FROM times_without_gap
+    -- the gs_* columns are not-NULL
+    -- gs_date,gs_hour,COALESCE(q.count,0) FROM times_without_gap
+LEFT JOIN q ON (q.date=gs_date AND q.hour=gs_hour);
+```
--- db_query.py
+++ db_query.py
@@ -44,7 +44,11 @@ def get_edit_count(cur, wiki = 'enwiki', title = 'UPS Airlines Flight 2976'):
         WITH q AS (
             SELECT
                DATE(ts_event_meta_dt) AS date, EXTRACT(HOUR FROM ts_event_meta_dt) AS hour,
-               COUNT(*)
+               COUNT(*),
+                -- To determine time range for zero-gap filling in result
+                -- MIN(ts_event_meta_dt) AS col_min, MAX(ts_event_meta_dt) AS col_max,
+                -- need start of hour, see https://www.postgresql.org/docs/current/functions-datetime.html
+                DATE_TRUNC('HOUR', MIN(ts_event_meta_dt)) AS col_min_startofhour
             FROM wiki_change_events_test
             WHERE
                event_type='edit' AND event_wiki=%s AND event_title=%s
@@ -58,19 +62,16 @@ def get_edit_count(cur, wiki = 'enwiki', title = 'UPS Airlines Flight 2976'):
                 DATE(gs) AS gs_date, EXTRACT(HOUR FROM gs) AS gs_hour
         FROM
                 generate_series(
-                    (SELECT MIN(date) FROM q),
-
-                    -- "1+": also generate rows for the final day having data (would be truncated otherwise)
-                    1+(SELECT MAX(date) FROM q),
+                    (SELECT MIN(col_min_startofhour) FROM q),
+                    (SELECT MAX(col_min_startofhour) FROM q),
                     interval '1 hour'
                 ) AS gs
-                -- TODO/FIXME: truncate this series at the last event in DB (otherwise there will be zero-padding at times later than latest value in DB)
         )
         SELECT
             -- column for development purposes in pgadmin
             -- gs_date,gs_hour,q.date,q.hour,COALESCE(q.count,-10) FROM times_without_gap
             -- the gs_* columns are not-NULL
-            gs_date,gs_hour,COALESCE(q.count,0) FROM times_without_gap
+            gs_date,gs_hour,COALESCE(q.count,-10) FROM times_without_gap
         LEFT JOIN q ON (q.date=gs_date AND q.hour=gs_hour);
         """,
         (q_wiki,q_title)
