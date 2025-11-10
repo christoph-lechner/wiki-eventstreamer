190d9d29ce92e40f4e446a7063c0edd6ef94c5e8
Christoph Lechner <cl@cl>
2025-11-10 20:07:37 +0000

    serial number in filenames now only 6 digits

--- streamreader/my_util.py
+++ streamreader/my_util.py
@@ -19,7 +19,7 @@ class FilenameGen:
         return do_switch
 
     def getfn(self):
-        s = self.datadir+'/stream_' + self.tnow.strftime('%Y%m%dT%H%M%S') + '_' + f'{self.seq:09d}'
+        s = self.datadir+'/stream_' + self.tnow.strftime('%Y%m%dT%H%M%S') + '_' + f'{self.seq:06d}'
         self.seq+=1
         return(s)
 
