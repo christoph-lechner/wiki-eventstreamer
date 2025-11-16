24a3aba76ae5dcdb6b4cfcf0a380a8eda98c5f6a
Christoph Lechner <cl@cl>
2025-11-16 18:11:55 +0100

    reworked decorator into classmethod

--- timedev.py
+++ timedev.py
@@ -6,29 +6,43 @@
 import datetime
 import time
 from functools import wraps
+from copy import deepcopy
 
-# global variable to measure time
-time_stats = []
+class MyTimer:
+    # global variable to measure time
+    time_stats = []
 
-def timeit(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        tnow = datetime.datetime.now()
-        tstart = time.perf_counter()
-        result = func(*args,**kwargs)
-        tend = time.perf_counter()
-        print(f'{func.__name__}: {tend-tstart:.6f} seconds')
-        time_stats.append({'tstart': tnow, 'func': func.__name__, 'dur': (tend-tstart)})
-        return result
-    return wrapper
+    @classmethod
+    def timeit(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            tnow = datetime.datetime.now()
+            tstart = time.perf_counter()
+            result = func(*args,**kwargs)
+            tend = time.perf_counter()
+            print(f'{func.__name__}: {tend-tstart:.6f} seconds')
+            self.time_stats.append({'tstart': tnow, 'func': func.__name__, 'dur': (tend-tstart)})
+            return result
+        return wrapper
 
-@timeit
+    @classmethod
+    def report1(self, f):
+        """
+        Function f is called for every bit of statistical info
+        FIXME: Should this also clear the collected statistics?
+        """
+        sorted_time_stats = sorted(self.time_stats, key=lambda obj: obj['tstart'])
+        for curr_stat in sorted_time_stats:
+            f(curr_stat)
+
+
+@MyTimer.timeit
 def do_something():
     # do ... important ... work
     time.sleep(2)
     return
 
-@timeit
+@MyTimer.timeit
 def do_work():
     # do ... important ... work
     time.sleep(2)
@@ -39,5 +53,10 @@ do_something()
 do_work()
 do_something()
 
+
+### process the results ###
+def report(s):
+    print(s)
+
 print('Collected time stats: ')
-print(time_stats)
+MyTimer.report1(report)
