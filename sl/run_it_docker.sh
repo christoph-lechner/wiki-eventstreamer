8191aa9d758730b875731f9fb80ab7a0c8c9309a
Christoph Lechner <cl@cl>
2025-11-16 17:23:11 +0100

    switched off analytics/telemetry traffic

--- sl/run_it_docker.sh
+++ sl/run_it_docker.sh
@@ -1,4 +1,18 @@
-
 #!/bin/sh
 
-sudo docker run -it -p 8501:8501 -v ./app:/app streamlit
+# Default settings:
+# Use statistics are collected and the program displays warning
+# "Collecting usage statistics. To deactivate, set browser.gatherUsageStats to false."
+#
+# The network debugging function of Firefox (or any other browser)
+# reveals that streamlit is sending data to webhooks.fivetran.com.
+# According to 
+#   https://discuss.streamlit.io/t/why-does-streamlit-send-webhook-requests-to-fivetran-com/87353/2
+# only analytics/telemetry data is sent, but let's switch it off anyhow.
+# If you use config.toml file (has to be placed in home directory of user running it, which is a bit complicated with docker-ized program), the settings are
+# [browser]
+# gatherUsageStats = false
+
+# But we can also do it on the command line
+
+sudo docker run -it -p 8501:8501 -v ./app:/app streamlit --browser.gatherUsageStats=False 
