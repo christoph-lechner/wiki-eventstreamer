e4418b330fefa0abee03dc0162989f428316c0ab
Christoph Lechner <cl@cl>
2025-11-27 00:26:07 +0100

    update to current DB structure

--- misc/db_conn.py
+++ misc/db_conn.py
@@ -1,14 +1,9 @@
 import psycopg
 
 def get_db_conn():
-    #conn = psycopg.connect(dbname = 'postgres', 
-    #                       user = 'postgres', 
-    #                       host= 'localhost',
-    #                       password = "postgres",
-    #                       port = 5432)
+    # password in ~/.pgpass (remember to set mode=600)
     conn = psycopg.connect(dbname = 'wikidb', 
-                           user = 'postgres', 
+                           user = 'wikiproj_ro', 
                            host= '192.168.2.253',
-                           password = "postgres",
                            port = 15432)
     return conn
