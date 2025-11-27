53de7b686a4ebabb842229cb7c2bed72bb390690
Christoph Lechner <cl@cl>
2025-11-27 14:56:08 +0100

    test that streamreader honours SIGTERM, resulting in controlled stop

---
+++ streamreader/test_sr.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import subprocess
+import threading
+import time
+import os
+import signal
+from pathlib import Path
+
+
+### First test: Check that streamreader reacts to SIGTERM ###
+def test_honors_SIGTERM():
+    def send_SIGTERM(pid, ignore_process_lookup_error=True):
+        time.sleep(5)
+        try:
+            os.kill(pid, signal.SIGTERM)
+        except ProcessLookupError as e:
+            if ignore_process_lookup_error:
+                pass # if we know that the process may not be there (anymore), ignore the error
+            else:
+                raise e
+
+    test_dir = Path(__file__).parent
+    path_sr = test_dir / 'wikistreamreader.py'
+    p = subprocess.Popen(
+        # useful for tests: sleep gives return code -15 (in Python) if SIGTERM is sent
+        # ['/bin/sleep', '100'],
+        [str(path_sr)],
+        cwd=test_dir # CWD = directory where the script lives
+        )
+
+    print(p.pid)
+
+    t = threading.Thread(target=send_SIGTERM, args=(p.pid,))
+    t.start()
+
+    # wait for process to finish
+    p.wait()
+    t.join()
+
+    rc = p.returncode
+    print(f'exit code was {rc}')
+    assert rc==0
+
