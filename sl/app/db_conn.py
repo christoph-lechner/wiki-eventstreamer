e948d7f344721bed3e3a88d7b4eb23969d806dd2
Christoph Lechner <cl@cl>
2026-01-06 23:09:50 +0100

    DB config for TLS verification ...
    Connection attempt fails if hostname is replaced by IP address -> good

--- sl/app/db_conn.py
+++ sl/app/db_conn.py
@@ -6,5 +6,6 @@ def get_db_conn():
                            user     = os.environ['DB_USER'], 
                            host     = os.environ['DB_HOST'],
                            password = os.environ['DB_PASSWORD'],
-                           port     = os.environ['DB_PORT'])
+                           port     = os.environ['DB_PORT'],
+                           sslmode='verify-full', sslrootcert='./isrgrootx1.pem')
     return conn
